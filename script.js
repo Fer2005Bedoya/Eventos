7f82e930329dddd2d252035ed1cbc91b29252dfb
Fer2005Bedoya <105678831+Fer2005Bedoya@users.noreply.github.com>
2022-08-10 16:28:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let boton = document.querySelector('button')
+let parrafo= document.querySelector('p')
+let contador= 0;
+let mover
+
+boton.addEventListener('click', ()=>{
+    
+})
+
+boton.addEventListener('click', ()=>{
+    boton.style.backgroundColor ='red'
+    contador= contador+1
+    parrafo.textContent= contador
+})
+boton.addEventListener('click' , ()=>{
+    alert('Hola')
+    boton.style.backgroundColor = 'purple'
+})
+console.log
